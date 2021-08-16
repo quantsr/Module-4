357ab6c3779c8128f597c4699dd07434886bf7ba
quantsr <48443553+quantsr@users.noreply.github.com>
2021-08-16 18:42:33 -0300

    Uploaded files from Module 4 Solution folder
    Uploaded the final result of the exercise. Getting ready to host everything on github

---
+++ SpeakGoodBye.js
@@ -0,0 +1,16 @@
+(function (window) {
+	var speakWord = "Good Bye";
+	var byeSpeaker = {};
+	byeSpeaker.speak = function (name){ 
+		console.log(speakWord + " " + name);
+	}
+	window.byeSpeaker = byeSpeaker;
+	})(window);
+
+
+
+
+
+
+
+
